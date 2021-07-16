8edebd51c45a77e0cb4b733253b3266ff511cbd8
Luca Longo <l.longo@ambita.it>
2021-07-16 16:28:27 +0200

    Minor changes

--- .php_cs.dist.php
+++ .php_cs.dist.php
@@ -3,10 +3,9 @@
 $finder = Symfony\Component\Finder\Finder::create()
     ->in([
         __DIR__ . '/src',
-        __DIR__ . '/tests',
+        // __DIR__ . '/tests',
     ])
     ->name('*.php')
-    ->notName('*.blade.php')
     ->ignoreDotFiles(true)
     ->ignoreVCS(true);
 
