cc1acbf46c89b7bbc0aa7b12c9596994512ac71a
Luca Longo <l.longo@ambita.it>
2021-07-19 16:56:12 +0200

    Minor changes

--- src/Concerns/ImplementsChecks.php
+++ src/Concerns/ImplementsChecks.php
@@ -7,6 +7,15 @@ use Masterix21\GreenPass\GreenPass;
 /** @mixin GreenPass */
 trait ImplementsChecks
 {
+    /**
+     * Verify if `$qrcode` is formally valid.
+     *
+     * WARNING: the check isn't enough to be sure that the Green Pass is valid.
+     *
+     * @param $qrcode
+     *
+     * @return bool
+     */
     public static function isFormallyValid($qrcode): bool
     {
         return str_starts_with($qrcode, 'HC1:');
